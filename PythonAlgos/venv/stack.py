4c85742afcdf11c3683a63d97f6d936b58f388f8
Jevgenijs Skinderskis (v-jskinderskis) <v-jskinderskis@expedia.com>
2019-04-25 15:20:47 -0700

    4242019

---
+++ PythonAlgos/venv/stack.py
@@ -0,0 +1,41 @@
+class Stack(list):
+
+    def __init__(self):
+        self.items = []
+
+
+    def isEmpty(self):
+        return self.items == []
+
+
+    def push(self, items):
+        self.items.append(items)
+
+
+    def pop(self):
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            raise Exception('Stack is empty!')
+
+
+    def peek(self):
+        return self.items[-1]
+
+
+    def size(self):
+        return len(self.items)
+
+def main():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    print(stack.size())
+    print(stack.peek())
+    print(stack.pop())
+    print(stack.peek())
+
+
+if __name__ == '__main__':
+    main()
---
+++ PythonAlgos/venv/work_with_arrays.py
@@ -0,0 +1,10 @@
+"""
+Lesson 5 by Timofey
+1. revert list
+[1,2,3,4,5] => [5,4,3,2,1]
+
+2. cyclic offset - left and right
+left [0,1,2,3,4] => [1,2,3,4,0]
+right [0,1,2,3,4] => [4,0,1,2,3]
+
+"""
